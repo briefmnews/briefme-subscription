afbacae9c22d9e0f99618dd9aa4963accd6d2b10
Thomas <thomas.carbillet@gmail.com>
2021-02-25 17:50:09 +0100

    feat: add AddCurrentSubscriptionMixin

--- briefme_subscription/views/mixins.py
+++ briefme_subscription/views/mixins.py
@@ -8,6 +8,30 @@ from ..chargify import ChargifyHelper
 logger = logging.getLogger(__name__)
 
 
+class AddCurrentSubscriptionMixin:
+    """Add the current subscription to context and as attribute."""
+
+    current_subscription = None
+    reset_subscription = True
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.current_subscription:
+            self.set_current_subscription()
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def set_current_subscription(self):
+        if self.request.user.is_authenticated:
+            self.current_subscription = self.request.user.current_subscription
+            if self.reset_subscription and self.current_subscription:
+                self.current_subscription.clear_chargify_subscription_cache()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_subscription"] = self.current_subscription
+        return context
+
+
 class ChargifyDirectMixin:
     chargify_helper = None
     chargify_api_call = None
