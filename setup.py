4b0d293a45d6fc43c4ca84d8444dca06b3e57a4e
Thomas <thomas.carbillet@gmail.com>
2021-03-17 11:40:31 +0100

    feat: release version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="briefme-subscription",
-    version="1.0.0",
+    version="1.1.0",
     description="Base subscription app for Brief.me projects",
     url="https://github.com/briefmnews/briefme-subscrition",
     author="Brief.me",
